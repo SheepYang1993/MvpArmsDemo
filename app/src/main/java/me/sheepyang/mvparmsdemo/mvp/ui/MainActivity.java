939838b832a614e997fd3ad1f29d205c9f2dcc80
sheepyang <332594623@qq.com>
2017-02-26 11:29:43 +0800

    :construction:

--- app/src/main/java/me/sheepyang/mvparmsdemo/mvp/ui/MainActivity.java
+++ app/src/main/java/me/sheepyang/mvparmsdemo/mvp/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package me.sheepyang.mvparmsdemo;
+package me.sheepyang.mvparmsdemo.mvp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import me.sheepyang.mvparmsdemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
